43919c8b2e74cfc16871af72ec0323264ac0994b
MeiQing <mq2553299@gmail.com>
2021-04-01 23:12:19 +0800

    complete 00

--- exoplayer-codelab-00/player-lib/src/main/java/com/example/exoplayer/PlayerActivity.java
+++ exoplayer-codelab-00/player-lib/src/main/java/com/example/exoplayer/PlayerActivity.java
@@ -11,13 +11,20 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
-* limitations under the License.
+ * limitations under the License.
  */
 package com.example.exoplayer;
 
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.exoplayer2.MediaItem;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.util.Util;
 
 
 /**
@@ -25,10 +32,90 @@ import android.os.Bundle;
  */
 public class PlayerActivity extends AppCompatActivity {
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_player);
-  }
+    private PlayerView playerView;
+    private SimpleExoPlayer player;
 
+    private boolean playWhenReady = true;   // 播放暂停的状态
+    private int currentWindow = 0;          // 当前窗口索引
+    private long playbackPosition = 0;      // 当前播放的位置
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player);
+
+        playerView = findViewById(R.id.video_view);
+    }
+
+    private void initializePlayer() {
+        player = new SimpleExoPlayer.Builder(this).build();
+        playerView.setPlayer(player);
+
+//        MediaItem mediaItem = MediaItem.fromUri(getString(R.string.media_url_mp3));
+        MediaItem mediaItem = MediaItem.fromUri(getString(R.string.media_url_mp4));
+        player.setMediaItem(mediaItem);
+
+        player.setPlayWhenReady(playWhenReady);
+        player.seekTo(currentWindow, playbackPosition);
+        player.prepare();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (Util.SDK_INT >= 24) {
+            initializePlayer();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (Util.SDK_INT >= 24) {
+            releasePlayer();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        hideSystemUi();
+        // Android API级别24和更高版本支持多个窗口。 由于您的应用是可见的，但在拆分窗口
+        // 模式下未激活，因此您需要在onStart上初始化播放器。 Android API 24级及更低版本
+        // 要求您等待尽可能长的时间，直到获取资源为止，因此要等到onResume才能初始化播放器。
+        if ((Util.SDK_INT < 24 || player == null)) {
+            initializePlayer();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // 对于API Level 24和更低的级别，不能保证会调用onStop，因此您必须在onPause中尽早释
+        // 放播放器。 使用API​Level 24或更高版本（带来了多窗口和拆分窗口模式），可以确保
+        // 调用onStop。 在暂停状态下，您的活动仍然可见，因此您需要等待释放播放器，直到onStop。
+        if (Util.SDK_INT < 24) {
+            releasePlayer();
+        }
+    }
+
+    @SuppressLint("InlinedApi")
+    private void hideSystemUi() {
+        playerView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+    }
+
+    private void releasePlayer() {
+        if (player != null) {
+            playWhenReady = player.getPlayWhenReady();
+            playbackPosition = player.getCurrentPosition();
+            currentWindow = player.getCurrentWindowIndex();
+            player.release();
+            player = null;
+        }
+    }
 }
